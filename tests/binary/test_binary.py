5e310a4dbc732ad44bf87aa6f54b4ff2e521d2e8
Jean Treibert <treibe_a@epita.fr>
2016-05-26 23:03:39 +0200

    Add(Tests): Add nyan cat to testsuit.

--- tests/binary/test_binary.py
+++ tests/binary/test_binary.py
@@ -3,14 +3,11 @@ from cffi import FFI
 from test_functions import *
 
 class TestBinary(unittest.TestCase):
-    def __init__(self, arg):
-        unittest.TestCase.__init__(self, arg)
-        self.ffi = FFI()
 
     def test_01_index(self):
         result = execute_cmd('./42sh -v')
-        self.assertEqual(result.stdout, "Version 0.5\n")
-
+        self.assertEqual("a", "ae")
+        #self.assertEqual(result.stdout, "Version 0.5\n")
 
     def test_02_expandable_options(self):
         result = execute_cmd('./42sh --ver')
@@ -18,4 +15,5 @@ class TestBinary(unittest.TestCase):
 
     def test_03_long_options(self):
         result = execute_cmd('./42sh --version')
-        self.assertEqual(result.stdout, "Version 0.5\n")
+        self.assertEqual("", "")
+        #self.assertTrue(result.stdout, "Version 0.5\n")
