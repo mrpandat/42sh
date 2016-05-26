9c99dc8e4555dc7df3c72e9bfc810cc516552fe8
Lucas Moisan <moisan_a@epita.fr>
2016-05-26 10:08:01 +0200

    Add(Tests): Add the possibility to test individual modules by giving arguments to the test suite

--- tests/test.py
+++ tests/test.py
@@ -8,42 +8,62 @@ from execute.test_exec import TestExec
 from utils.test_utils import TestUtils
 
 
-def launch_all():
-
-    utils_suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestUtils)
+def launch_parser_tests():
     parser_suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestParser)
-    lexer_suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestLexer)
-    exec_suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestExec)
-
-    print("Launching utils tests....")
-    unittest.TextTestRunner().run(utils_suite)
-    print("Launching parser tests....")
+    print(" Launching parser tests ".center(80, '*'))
     unittest.TextTestRunner().run(parser_suite)
-    print("Launching lexer tests....")
+
+
+def launch_lexer_tests():
+    lexer_suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestLexer)
+    print(" Launching lexer tests ".center(80, '*'))
     unittest.TextTestRunner().run(lexer_suite)
-    print("Launching execution tests....")
+
+
+def launch_exec_tests():
+    exec_suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestExec)
+    print(" Launching execution tests ".center(80, '*'))
     unittest.TextTestRunner().run(exec_suite)
-    
-    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+
+def launch_utils_tests():
+    utils_suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestUtils)
+    print(" Launching utils tests ".center(80, '*'))
+    unittest.TextTestRunner().run(utils_suite)
+
+
+def launch_all():
+    launch_utils_tests()
+    launch_lexer_tests()
+    launch_parser_tests()
+    launch_exec_tests()
+
 
 if __name__ == "__main__":
     sys.argv[0] = ""
     for arg in sys.argv:
-        if arg=="":
+        if arg == "":
             continue
-        elif arg == "-l" or arg =="--list":
-            print ("categories")
+        elif arg == "-l" or arg == "--list":
+            print("categories")
             exit(0)
         elif arg == "-c":
             print("execute category")
             exit(0)
         elif arg == "-s" or arg=="--sanity":
-            print ("valgrind")
+            print("valgrind")
             exit(0)
-        else:
-            print ("Unknow option : " + arg)
-            exit(1)
-    launch_all()
+    if any(["utils", "lexer", "parser", "execute"]) in sys.argv:
+        if "utils" in sys.argv:
+            launch_utils_tests()
+        if "lexer" in sys.argv:
+            launch_lexer_tests()
+        if "parser" in sys.argv:
+            launch_parser_tests()
+        if "execute" in sys.argv:
+            launch_exec_tests()
+    else:
+        launch_all()
 
 
 
