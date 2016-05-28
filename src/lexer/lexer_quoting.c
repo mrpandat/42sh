9596b439d1c75342bef3d97cfa14315b414b94d6
Thomas Domingues <doming_t@epita.fr>
2016-05-29 00:54:37 +0200

    Feat(Lexer): Implemented double quote matching feature.

--- src/lexer/lexer_quoting.c
+++ src/lexer/lexer_quoting.c
@@ -37,20 +37,26 @@ bool lexer_match_quote(struct s_lexer *lexer)
 
     size_t len = 0;
     char *val = NULL;
-    char *copy = lexer->current;
+    char *copy = NULL;
 
-    while (*copy && 0 != strncmp(copy, "'", strlen("'")))
+    if (0 == strncmp(lexer->current, "'", strlen("'")))
     {
-        len++;
-        copy++;
+        lexer->current++;
+        copy = lexer->current;
+
+        while (*copy && 0 != strncmp(copy, "'", strlen("'")))
+        {
+            len++;
+            copy++;
+        }
     }
 
     val = strndup(lexer->current, len);
 
     if (NULL != val && strlen(val) > 0)
     {
-        lexer_add_token(lexer, TK_WORD, val);
-        lexer->current += strlen(val);
+        lexer_add_token(lexer, TK_ESC_WORD, val);
+        lexer->current += strlen(val) + 1;
         return true;
     }
 
@@ -62,21 +68,32 @@ bool lexer_match_dquote(struct s_lexer *lexer)
     if (NULL == lexer || NULL == lexer->current || !*lexer->current)
         return false;
 
-    size_t len = 0;
     char *val = NULL;
-    char *copy = lexer->current;
 
     if (0 == strncmp(lexer->current, "\"", strlen("\"")))
     {
         lexer->current++;
+        char *copy = lexer->current;
 
-        while (*copy && 0 != strncmp(copy, "'", strlen("'")))
+        while (*copy && 0 != strncmp(copy, "\"", strlen("\"")))
         {
-            len++;
+            if (0 == strncmp(lexer->current, "\\\"", strlen("\\\"")))
+            {
+                copy += strlen("\\\"");
+                continue;
+            }
             copy++;
         }
+
+        val = strndup(lexer->current, copy - lexer->current);
     }
 
+    if (NULL != val && strlen(val) > 0)
+    {
+        lexer_add_token(lexer, TK_WORD, val);
+        lexer->current += strlen(val) + 1;
+        return true;
+    }
     return false;
 }
 
