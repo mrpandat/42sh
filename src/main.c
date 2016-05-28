a45f9202251c4890fe92ae3c0768540830edfb1d
Martin Lees <lees_m@epita.fr>
2016-05-28 17:57:46 +0200

    Add(Execute): Adds a few more tests for the argument parser

--- src/main.c
+++ src/main.c
@@ -5,13 +5,7 @@
 int main(int argc, char *argv[]) {
     struct options opt = {0, 0, 0, "", "", ""};
     parse_options(argc, argv, &opt, 1);
-    if (strcmp(opt.command, "") == 0) {
-        print_exit(-1, "42sh [ GNU long options ] [ options ] [ file ]",
-                   stderr);
-        return -1;
-    }
-
-    execute(opt);
+     execute(opt);
     return 0;
 }
 
--- tests/binary/test_binary.py
+++ tests/binary/test_binary.py
@@ -15,3 +15,9 @@ class TestBinary(unittest.TestCase):
     def test_03_long_options(self):
         result = execute_cmd('./42sh --version')
         self.assertTrue(result.stdout, "Version 0.5\n")
+
+    def test_04_help(self):
+        result = execute_cmd('./42sh -h')
+        self.assertTrue(result.stdout, "42sh [ GNU long options ] [ options ] [ file ]\n")
+        self.assertTrue(result.returncode, 0)
+
