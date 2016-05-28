785e07f6e4fd24856b9e6f96431f6f940ae3cf87
Jean Treibert <treibe_a@epita.fr>
2016-05-28 17:42:44 +0200

    Fix(Tests): Fix binary tests, wrong folder.

--- tests/binary/test_binary.py
+++ tests/binary/test_binary.py
@@ -1,17 +1,16 @@
 import unittest
-from cffi import FFI
 from test_functions import *
 
 class TestBinary(unittest.TestCase):
 
     def test_01_index(self):
-        result = execute_cmd('./42sh -v')
+        result = execute_cmd('../42sh -v')
         self.assertEqual(result.stdout, "Version 0.5\n")
 
     def test_02_expandable_options(self):
-        result = execute_cmd('./42sh --ver')
+        result = execute_cmd('../42sh --ver')
         self.assertEqual(result.stdout, "Version 0.5\n")
 
     def test_03_long_options(self):
-        result = execute_cmd('./42sh --version')
+        result = execute_cmd('../42sh --version')
         self.assertTrue(result.stdout, "Version 0.5\n")
