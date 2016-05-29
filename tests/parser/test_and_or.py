5e0d00390b7aaaa6b314dc2b3e7e316d8b4cb2e7
Lucas Moisan <moisan_a@epita.fr>
2016-05-29 15:04:51 +0200

    Add(Parser): Add tests about and or nodes.

--- tests/parser/test_and_or.py
+++ tests/parser/test_and_or.py
@@ -58,13 +58,27 @@ class TestAndOr(unittest.TestCase):
                 node,
                 self.init_and_process_lexer(b'myword || myword || myword ||')))
 
-    def test_07_node_attributes(self):
+    def test_07_or_node_attributes(self):
         node = self.lib.init_ast_node()
         self.lib.read_and_or(node, self.init_and_process_lexer(b'word1 || word2'))
         and_or_1 = node.data.s_and_or_node
         command_1 = and_or_1.left.data.s_pipeline_node.commands[0].data \
             .s_command_node.content.data.s_simple_command_node
         self.assertEqual(and_or_1.type, self.lib.ND_OR_IF)
+        self.assertEqual(self.ffi.string(command_1.elements[0].data.word), b'word1')
+        and_or_2 = node.data.s_and_or_node.right.data.s_and_or_node
+        command_2 = and_or_2.left.data.s_pipeline_node.commands[0].data \
+            .s_command_node.content.data.s_simple_command_node
+        self.assertEqual(and_or_2.type, self.lib.ND_IF_NONE)
+        self.assertEqual(self.ffi.string(command_2.elements[0].data.word), b'word2')
+
+    def test_08_and_node_attributes(self):
+        node = self.lib.init_ast_node()
+        self.lib.read_and_or(node, self.init_and_process_lexer(b'word1 && word2'))
+        and_or_1 = node.data.s_and_or_node
+        command_1 = and_or_1.left.data.s_pipeline_node.commands[0].data \
+            .s_command_node.content.data.s_simple_command_node
+        self.assertEqual(and_or_1.type, self.lib.ND_AND_IF)
         self.assertEqual(self.ffi.string(command_1.elements[0].data.word), b'word1')
         and_or_2 = node.data.s_and_or_node.right.data.s_and_or_node
         command_2 = and_or_2.left.data.s_pipeline_node.commands[0].data \
