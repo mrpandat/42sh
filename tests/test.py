ba1521130359fa4c6a984b58a0071106bbd1c111
Jean Treibert <treibe_a@epita.fr>
2016-06-03 17:11:36 +0200

    Add(Tests): Add git.html which list commiters in html.

--- tests/test.py
+++ tests/test.py
@@ -131,8 +131,13 @@ def print_nyan():
 
 def tracegraph():
     print("Generating reports...")
-    print(execute_cmd("pwd"))
-
+    a = execute_cmd("git shortlog -s -n")
+    b = ""
+    for line in a.stdout.splitlines():
+        b += ("<h1>" + line + "</h1>")
+    text_file = open("../doc/git.html", "w")
+    text_file.write(b)
+    text_file.close()
     fig = {
         'data': [{'labels': ['Errors', 'Failures', 'Success'],
                   'values': [nb_errors, nb_fail, nb_success],
