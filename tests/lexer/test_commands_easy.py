3963a11c490a8b3b181b177af912ecc3f5c18b93
Thomas Domingues <doming_t@epita.fr>
2016-06-05 15:34:25 +0200

    Test(Lexer): Added simple operator checking tests.

--- tests/lexer/test_commands_easy.py
+++ tests/lexer/test_commands_easy.py
@@ -20,9 +20,56 @@ class TestCommandsEasy(unittest.TestCase):
         init_and_process_lexer(self, b'echo test')
 
         self.assertTrue(
-            list_equals(self,
+            list_equals(self, False,
                         (self.lib.TK_WORD, b'echo'),
                         (self.lib.TK_WORD, b'test'),
+                        (self.lib.TK_EOF, b'EOF'))
+        )
+
+        destroy_lexer(self)
+
+    def test_02_lex_simple_and_op(self):
+        init_and_process_lexer(self, b'echo test && cat -e toto.txt')
+
+        self.assertTrue(
+            list_equals(self, False,
+                        (self.lib.TK_WORD, b'echo'),
+                        (self.lib.TK_WORD, b'test'),
+                        (self.lib.TK_AND_IF, b'&&'),
+                        (self.lib.TK_WORD, b'cat'),
+                        (self.lib.TK_WORD, b'-e'),
+                        (self.lib.TK_WORD, b'toto.txt'),
+                        (self.lib.TK_EOF, b'EOF'))
+        )
+
+        destroy_lexer(self)
+
+    def test_03_lex_simple_or_op(self):
+        init_and_process_lexer(self, b'test -e AUTHORS || echo File not found.')
+
+        self.assertTrue(
+            list_equals(self, False,
+                        (self.lib.TK_WORD, b'test'),
+                        (self.lib.TK_WORD, b'-e'),
+                        (self.lib.TK_WORD, b'AUTHORS'),
+                        (self.lib.TK_OR_IF, b'||'),
+                        (self.lib.TK_WORD, b'echo'),
+                        (self.lib.TK_WORD, b'File'),
+                        (self.lib.TK_WORD, b'not'),
+                        (self.lib.TK_WORD, b'found.'),
+                        (self.lib.TK_EOF, b'EOF'))
+        )
+
+        destroy_lexer(self)
+
+    def test_04_lex_simple_newline(self):
+        init_and_process_lexer(self, b'cat TODO\n')
+
+        self.assertTrue(
+            list_equals(self, False,
+                        (self.lib.TK_WORD, b'cat'),
+                        (self.lib.TK_WORD, b'TODO'),
+                        (self.lib.TK_NEWLINE, b'\n'),
                         (self.lib.TK_EOF, b'EOF'))
         )
 
