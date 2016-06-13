4a47ce9de7226c3b40ccbcb43ab529edd9800aca
Jean Treibert <treibe_a@epita.fr>
2016-06-13 20:39:23 +0200

    Feat(Path-expansion): Working version of path expansion.

--- src/expansion/path_expansion.c
+++ src/expansion/path_expansion.c
@@ -1,35 +1,25 @@
 #include <stdio.h>
-#include <sys/types.h>
 #include <dirent.h>
 #include "../includes/global.h"
 #include "../includes/expansion.h"
-#include "../includes/util.h"
-#include <fnmatch.h>
+
 
 #define _GNU_SOURCE
 
 
 char *expand_path(char *path)
 {
-    DIR *dp = opendir("./");
+    DIR *dp = opendir(".");
     struct dirent *ep;
-    if (dp != NULL)
+    if (dp != NULL && path != NULL)
     {
-        while ((ep = readdir(dp)) != NULL){
+        while ((ep = readdir(dp)) != NULL)
+        {
             puts(ep->d_name);
-            puts("begin");
             if (!fnmatch(path, (ep->d_name), 0))
-            {
-                puts("ok\n");
                 puts(ep->d_name);
-            }
-            puts("begin");
-
-            (void) closedir(dp);
         }
+        closedir(dp);
     }
-    else
-        perror("Couldn't open the directory");
-    puts("done");
     return "lol";
 }
