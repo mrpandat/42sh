35305452cf5fe8e239c9a8dd23fb04a0d1439c1c
Jean Treibert <treibe_a@epita.fr>
2016-06-14 16:28:41 +0200

    Add(Variables): Add new header variables.

---
+++ src/includes/variables.h
@@ -0,0 +1,16 @@
+/**
+ ** @file variables.h
+ ** @brief variables for 42sh
+ ** @author Treibert Jean
+ */
+
+
+#ifndef INC_42SH_VAR_H
+#define INC_42SH_VAR_H
+
+#include "global.h"
+
+int is_var(char *word);
+
+
+#endif
